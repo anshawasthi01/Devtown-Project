6f17b4d800ef59b96eda5a0fa68f0f596c54dfe3
ANSH AWASTHI <95968189+anshawasthi01@users.noreply.github.com>
2022-07-07 03:06:36 +0530

    Add files via upload

---
+++ Note Taking App Using Javascript & ReactJS/src/components/App.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+import Header from "./Header";
+import Footer from "./Footer";
+import Notes from "./Notes";
+
+function App() {
+  return (
+    <div>
+      <Header />
+      <Notes />
+      <Notes />
+      <Notes />
+      <Notes />
+      <Notes />
+      <Notes />
+      <Notes />
+      <Notes />
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
---
+++ Note Taking App Using Javascript & ReactJS/src/components/Notes.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+function Notes() {
+  return (
+    <div className="note">
+      <h1>This is the React Bootcamp</h1>
+      <p>
+        All the students who complete the Bootcamp with complete attendance and
+        project submission will be rewarded certificates from us.
+      </p>
+    </div>
+  );
+}
+
+export default Notes;

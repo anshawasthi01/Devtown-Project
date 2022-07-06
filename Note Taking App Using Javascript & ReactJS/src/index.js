f4eae832cce0006c9b60135c8580e12f8b1e9d47
ANSH AWASTHI <95968189+anshawasthi01@users.noreply.github.com>
2022-07-07 00:22:33 +0530

    Create index.js

---
+++ Note Taking App Using Javascript & ReactJS/src/index.js
@@ -0,0 +1,11 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import Heading from "./Heading";
+
+ReactDOM.render(
+  <div>
+    <Heading></Heading>
+    <p>I'm Ansh Awasthi</p>
+  </div>,
+  document.getElementById("root")
+);
